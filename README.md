1998d7343d93d232671bfe01a3287e054b92ab3f
luccorrea <correa.lucas1994@gmail.com>
2021-12-21 19:25:28 -0300

    commit final

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ezops-test-lucas
 Prova para DevOps Jr ezops
 
+Seguindo esse tutorial
 https://betterprogramming.pub/simple-chat-application-in-node-js-using-express-mongoose-and-socket-io-ee62d94f5804
+
+Porem como nao pude utilizar o mLab, pois ele nao esta mais criando novas contas. Utilizei o mongodb atlas
--- server.js
+++ server.js
@@ -1,10 +1,70 @@
 var express = require('express');
-
+var bodyParser = require('body-parser')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
+var dbUrl = 'mongodb://lucas:12345@cluster0-shard-00-00.qr5jt.mongodb.net:27017,cluster0-shard-00-01.qr5jt.mongodb.net:27017,cluster0-shard-00-02.qr5jt.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mhxmpv-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
 
 
-var server = app.listen(3000, () => {
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  try{
+    var message = new Message(req.body);
+
+    var savedMessage = await message.save()
+      console.log('saved');
+
+    var censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('message', req.body);
+      res.sendStatus(200);
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('error',error);
+  }
+  finally{
+    console.log('Message Posted')
+  }
+
+})
+
+
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl ,{useNewUrlParser: true, useUnifiedTopology: true} ,(err) => {
+  console.log('mongodb connected',err);
+})
+
+var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
 });
